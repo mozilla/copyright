83954e6852c1e08d96e5e3265c961cff13c17810
ScottDowne <scott.downe@gmail.com>
2017-02-24 11:13:15 -0500

    layout form and css.

--- src/components/form-body.js
+++ src/components/form-body.js
@@ -6,40 +6,42 @@ module.exports = React.createClass({
   },
   render: function() {
     return (
-      <div className="form-body">
-        <div className="form-description">
-          {this.context.intl.formatMessage({id: 'main_description'})}
+      <div className="form-body-container">
+        <div className="form-body">
+          <div className="form-description">
+            {this.context.intl.formatMessage({id: 'main_description'})}
+          </div>
+          <h4>
+            {this.context.intl.formatMessage({id: 'question_1'})}
+          </h4>
+          <p>
+            {this.context.intl.formatMessage({id: 'answer_1'})}
+          </p>
+          <h4>
+            {this.context.intl.formatMessage({id: 'question_2'})}
+          </h4>
+          <p>
+            {this.context.intl.formatMessage({id: 'answer_2'})}
+          </p>
+          <h4>
+            {this.context.intl.formatMessage({id: 'question_3'})}
+          </h4>
+          <p>
+            {this.context.intl.formatMessage({id: 'answer_3'})}
+          </p>
+          <h4>
+            {this.context.intl.formatMessage({id: 'copyright_matters'})}
+          </h4>
+          <h3>
+            {this.context.intl.formatMessage({id: 'now_what_title'})}
+          </h3>
+          <h4>
+            {this.context.intl.formatMessage({id: 'now_what_tagline'})}
+          </h4>
+          <p>
+            {this.context.intl.formatMessage({id: 'now_what_description'})}
+          </p>
         </div>
-        <h4>
-          {this.context.intl.formatMessage({id: 'question_1'})}
-        </h4>
-        <p>
-          {this.context.intl.formatMessage({id: 'answer_1'})}
-        </p>
-        <h4>
-          {this.context.intl.formatMessage({id: 'question_2'})}
-        </h4>
-        <p>
-          {this.context.intl.formatMessage({id: 'answer_2'})}
-        </p>
-        <h4>
-          {this.context.intl.formatMessage({id: 'question_3'})}
-        </h4>
-        <p>
-          {this.context.intl.formatMessage({id: 'answer_3'})}
-        </p>
-        <h4>
-          {this.context.intl.formatMessage({id: 'copyright_matters'})}
-        </h4>
-        <h3>
-          {this.context.intl.formatMessage({id: 'now_what_title'})}
-        </h3>
-        <h4>
-          {this.context.intl.formatMessage({id: 'now_what_tagline'})}
-        </h4>
-        <p>
-          {this.context.intl.formatMessage({id: 'now_what_description'})}
-        </p>
       </div>
     );
   }
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -7,41 +7,43 @@ module.exports = React.createClass({
   },
   render: function() {
     return (
-      <div className="form-body">
-        <h3>
-          {this.context.intl.formatMessage({id: 'take_action_headline'})}
-        </h3>
-        <p>
-          {this.context.intl.formatMessage({id: 'take_action_description'})}
-        </p>
-        <label>
-          {this.context.intl.formatMessage({id: 'first_name'})}
-        </label>
-        <input/>
-        <label>
-          {this.context.intl.formatMessage({id: 'last_name'})}
-        </label>
-        <input/>
-        <label>
-          {this.context.intl.formatMessage({id: 'email'})}
-        </label>
-        <input/>
-        <label>
-          {this.context.intl.formatMessage({id: 'country'})}
-        </label>
-        <input/>
-        <div className="privacy-policy">
-          <FormattedMessage
-            id='sign_up_notice'
-            values={{
-              linkTerms: (<a href="#">{this.context.intl.formatMessage({id: 'link_tos'})}</a>),
-              linkPrivacyPolicy: (<a href="#">{this.context.intl.formatMessage({id: 'link_pp'})}</a>)
-            }}
-          />
+      <div className="signup-form-container">
+        <div className="signup-form">
+          <h3>
+            {this.context.intl.formatMessage({id: 'take_action_headline'})}
+          </h3>
+          <p>
+            {this.context.intl.formatMessage({id: 'take_action_description'})}
+          </p>
+          <label>
+            {this.context.intl.formatMessage({id: 'first_name'})}
+          </label>
+          <input/>
+          <label>
+            {this.context.intl.formatMessage({id: 'last_name'})}
+          </label>
+          <input/>
+          <label>
+            {this.context.intl.formatMessage({id: 'email'})}
+          </label>
+          <input/>
+          <label>
+            {this.context.intl.formatMessage({id: 'country'})}
+          </label>
+          <input/>
+          <div className="privacy-policy">
+            <FormattedMessage
+              id='sign_up_notice'
+              values={{
+                linkTerms: (<a href="#">{this.context.intl.formatMessage({id: 'link_tos'})}</a>),
+                linkPrivacyPolicy: (<a href="#">{this.context.intl.formatMessage({id: 'link_pp'})}</a>)
+              }}
+            />
+          </div>
+          <button>
+            {this.context.intl.formatMessage({id: 'sign_up_button'})}
+          </button>
         </div>
-        <button>
-          {this.context.intl.formatMessage({id: 'sign_up_button'})}
-        </button>
       </div>
     );
   }
