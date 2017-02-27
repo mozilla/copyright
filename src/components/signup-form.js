004518ca2a8cb4a403100fd93b68c1dffa07e69e
ScottDowne <scott.downe@gmail.com>
2017-02-26 23:36:44 -0500

    Lock down state

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { setEmailError, setEmail, setFirstName, setLastName, setCountry } from '../actions';
+import { connect } from 'react-redux';
 
-module.exports = React.createClass({
+var Signup = React.createClass({
   contextTypes: {
     intl: React.PropTypes.object
+  },
+  firstNameChange: function(e) {
+    this.props.setFirstName(e.target.value);
+  },
+  lastNameChange: function(e) {
+    this.props.setLastName(e.target.value);
+  },
+  emailChange: function(e) {
+    this.props.setEmail(e.target.value);
+  },
+  countryChange: function(e) {
+    this.props.setCountry(e.target.value);
   },
   render: function() {
     return (
@@ -16,10 +30,10 @@ module.exports = React.createClass({
           <p>
             {this.context.intl.formatMessage({id: 'take_action_description'})}
           </p>
-          <input placeholder={this.context.intl.formatMessage({id: 'first_name'})}/>
-          <input placeholder={this.context.intl.formatMessage({id: 'last_name'})}/>
-          <input placeholder={this.context.intl.formatMessage({id: 'email'})}/>
-          <input placeholder={this.context.intl.formatMessage({id: 'country'})}/>
+          <input value={this.props.firstName} onChange={this.firstNameChange} placeholder={this.context.intl.formatMessage({id: 'first_name'})}/>
+          <input value={this.props.lastName} onChange={this.lastNameChange} placeholder={this.context.intl.formatMessage({id: 'last_name'})}/>
+          <input value={this.props.email} onChange={this.emailChange} type="email" required placeholder={this.context.intl.formatMessage({id: 'email'})}/>
+          <input value={this.props.country} onChange={this.countryChange} placeholder={this.context.intl.formatMessage({id: 'country'})}/>
           <p className="privacy-policy">
             <FormattedMessage
               id='sign_up_notice'
@@ -37,3 +51,33 @@ module.exports = React.createClass({
     );
   }
 });
+
+module.exports = connect(
+function(state) {
+  return {
+    email: state.signupForm.email,
+    emailError: state.signupForm.emailError,
+    firstName: state.signupForm.firstName,
+    lastName: state.signupForm.lastName,
+    country: state.signupForm.country
+  };
+},
+function(dispatch) {
+  return {
+    setEmailError: function(data) {
+      dispatch(setEmailError(data));
+    },
+    setEmail: function(data) {
+      dispatch(setEmail(data));
+    },
+    setFirstName: function(data) {
+      dispatch(setFirstName(data));
+    },
+    setLastName: function(data) {
+      dispatch(setLastName(data));
+    },
+    setCountry: function(data) {
+      dispatch(setCountry(data));
+    }
+  };
+})(Signup);
