9393aaffa01abe5727b9ebbf7c71232d885d1116
ScottDowne <scott.downe@gmail.com>
2017-03-08 14:01:00 -0500

    Share page without buttons.

--- src/pages/share.js
+++ src/pages/share.js
@@ -8,7 +8,7 @@ var Signup = React.createClass({
     intl: React.PropTypes.object
   },
   render: function() {
-    var className = "signup";
+    var className = "share";
     if (this.props.test) {
       className += " " + this.props.test;
     }
@@ -17,8 +17,18 @@ var Signup = React.createClass({
         <div className="page">
           <SimpleNav/>
           <div className="share-page-container">
-            <div className="share-page">
+            <div className="share-page-content-container">
+              <div className="share-page-content">
+                <h2>
+                  {this.context.intl.formatMessage({id: 'title'})}
+                </h2>
+                <p>
+                  {this.context.intl.formatMessage({id: 'headline'})}
+                </p>
+                <div className="share-buttons">
 
+                </div>
+              </div>
             </div>
           </div>
         </div>
