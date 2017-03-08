0322ad9fdb9761f3d7fe9ebd1be70ac40a448c3b
ScottDowne <scott.downe@gmail.com>
2017-03-07 23:43:56 -0500

    Updates for checkbox

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,22 @@ export function setEmailError(data) {
   return { type: 'SET_EMAIL_ERROR', data };
 }
 
+export function setSignupCheckbox(data) {
+  return { type: 'SET_SIGNUP_CHECKBOX', data };
+}
+
+export function setSignupCheckboxError(data) {
+  return { type: 'SET_SIGNUP_CHECKBOX_ERROR', data };
+}
+
+export function setPrivacyCheckbox(data) {
+  return { type: 'SET_PRIVACY_CHECKBOX', data };
+}
+
+export function setPrivacyCheckboxError(data) {
+  return { type: 'SET_PRIVACY_CHECKBOX_ERROR', data };
+}
+
 export function setFirstName(data) {
   return { type: 'SET_FIRST_NAME', data };
 }
--- src/reducers/signup-form.js
+++ src/reducers/signup-form.js
@@ -3,6 +3,10 @@ import assign from 'object-assign';
 const initialState = {
   email: ``,
   emailError: ``,
+  signupCheckbox: false,
+  signupCheckboxError: ``,
+  privacyCheckbox: false,
+  privacyCheckboxError: ``,
   firstName: ``,
   lastName: ``,
   country: ``
@@ -18,6 +22,24 @@ const signupApp = (state = initialState, action) => {
   case 'SET_EMAIL_ERROR':
     return assign({}, state, {
       emailError: action.data
+    });
+  case 'SET_SIGNUP_CHECKBOX':
+    return assign({}, state, {
+      signupCheckbox: action.data,
+      signupCheckboxError: ""
+    });
+  case 'SET_SIGNUP_CHECKBOX_ERROR':
+    return assign({}, state, {
+      signupCheckboxError: action.data
+    });
+  case 'SET_PRIVACY_CHECKBOX':
+    return assign({}, state, {
+      privacyCheckbox: action.data,
+      privacyCheckboxError: ""
+    });
+  case 'SET_PRIVACY_CHECKBOX_ERROR':
+    return assign({}, state, {
+      privacyCheckboxError: action.data
     });
   case 'SET_FIRST_NAME':
     return assign({}, state, {
