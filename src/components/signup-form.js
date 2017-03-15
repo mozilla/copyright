0ec2dee8070f96cd8dddbca72328aeeb015a66d9
Théo Chevalier <theo.chevalier11@gmail.com>
2017-03-15 04:16:37 +0000

    Localize country list

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -78,6 +78,14 @@ var Signup = React.createClass({
     return this.stickyContainer.getClientRects()[0].top + this.stickyContent.offsetHeight + window.scrollY - window.innerHeight;
   },
   render: function() {
+    var englishCountries = require('localized-countries')(this.context.intl.locale);
+    var countryList = ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB'];
+    var localizedCountries = {};
+
+    for (var i = 0; i < countryList.length; i++) {
+      localizedCountries[englishCountries.get(countryList[i])] = countryList[i];
+    }
+
     var emailClassName = classnames({
       "invalid": !!this.props.emailError
     });
@@ -113,35 +121,12 @@ var Signup = React.createClass({
           <input autoComplete="off" ref={(input) => { this.emailInput = input; }} type='email' className={emailClassName} value={this.props.email} onChange={this.emailChange} required placeholder={this.context.intl.formatMessage({id: 'email'})}/>
           <select autoComplete="off" required value={this.props.country} onChange={this.countryChange}>
             <option value="">{this.context.intl.formatMessage({id: 'country'})}</option>
-            <option value="AT">Austria</option>
-            <option value="BE">Belgium</option>
-            <option value="BG">Bulgaria</option>
-            <option value="HR">Croatia</option>
-            <option value="CY">Cyprus</option>
-            <option value="CZ">Czech Republic</option>
-            <option value="DK">Denmark</option>
-            <option value="EE">Estonia</option>
-            <option value="FI">Finland</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-            <option value="GR">Greece</option>
-            <option value="HU">Hungary</option>
-            <option value="IE">Ireland</option>
-            <option value="IT">Italy</option>
-            <option value="LV">Latvia</option>
-            <option value="LT">Lithuania</option>
-            <option value="LU">Luxembourg</option>
-            <option value="MT">Malta</option>
-            <option value="NL">Netherlands</option>
-            <option value="PL">Poland</option>
-            <option value="PT">Portugal</option>
-            <option value="RO">Romania</option>
-            <option value="SK">Slovakia</option>
-            <option value="SI">Slovenia</option>
-            <option value="ES">Spain</option>
-            <option value="SE">Sweden</option>
-            <option value="GB">United Kingdom</option>
-            <option value="other" data-other="">Other</option>
+            {
+              Object.keys(localizedCountries).sort().map(function(name, index) {
+                return <option key={localizedCountries[name]} value={localizedCountries[name]}>{name}</option>;
+              })
+            }
+            <option value="other" data-other="">{this.context.intl.formatMessage({id: 'country_other'})}</option>
           </select>
           <p className="error-message">{this.props.emailError}</p>
           <p className="error-message">{this.state.signupError}</p>
