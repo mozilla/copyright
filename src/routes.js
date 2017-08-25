d6817d1ed7dbd24f0842eb00fd92f8c0761ba4b4
Pomax <pomax@nihongoresources.com>
2017-08-25 12:09:10 -0700

    superfluous spaces"

--- src/routes.js
+++ src/routes.js
@@ -10,15 +10,15 @@ var routes = (
     {
       Object.keys(locales).map(function(locale) {
         return (
-            <Route key={locale} path={locale}>
-              <IndexRoute component={require('./pages/home.js')}/>
-              <Route path='home' component={require('./pages/home.js')}/>
-              <Route path='impact' component={require('./pages/impact.js')}/>
-              <Route path='call-now' component={require('./pages/call-now.js')}/>
-              <Route path='share' component={require('./pages/share.js')}/>
-              <Route path='resources' component={require('./pages/resources.js')}/>
-              <Redirect from="*" to={"/" + locale} />
-            </Route>
+          <Route key={locale} path={locale}>
+            <IndexRoute component={require('./pages/home.js')}/>
+            <Route path='home' component={require('./pages/home.js')}/>
+            <Route path='impact' component={require('./pages/impact.js')}/>
+            <Route path='call-now' component={require('./pages/call-now.js')}/>
+            <Route path='share' component={require('./pages/share.js')}/>
+            <Route path='resources' component={require('./pages/resources.js')}/>
+            <Redirect from="*" to={"/" + locale} />
+          </Route>
         );
       })
     }
