f609bd40d64f7e9f4fc6ec47815c341048e7781d
ScottDowne <scott.downe@gmail.com>
2017-08-25 11:18:01 -0400

    Few bugs in locale data.

--- src/data/locales.js
+++ src/data/locales.js
@@ -1,27 +1,7 @@
 module.exports = [
   {
-      description: 'Asturianu',
-      code: 'ast'
-  },
-  {
-      description: 'Български',
-      code: 'bg'
-  },
-  {
-      description: 'Čeština',
-      code: 'cs'
-  },
-  {
-      description: 'Dansk',
-      code: 'da'
-  },
-  {
       description: 'Deutsch',
       code: 'de'
-  },
-  {
-      description: 'Ελληνικά',
-      code: 'el'
   },
   {
       description: 'English (US)',
@@ -32,79 +12,15 @@ module.exports = [
       code: 'es'
   },
   {
-      description: 'Eesti keel',
-      code: 'et'
-  },
-  {
-      description: 'suomi',
-      code: 'fi'
-  },
-  {
       description: 'Français',
       code: 'fr'
-  },
-  {
-      description: 'Frysk',
-      code: 'fy-NL'
-  },
-  {
-      description: 'Gaeilge',
-      code: 'ga-IE'
-  },
-  {
-      description: 'Hrvatski',
-      code: 'hr'
-  },
-  {
-      description: 'magyar',
-      code: 'hu'
   },
   {
       description: 'Italiano',
       code: 'it'
   },
   {
-      description: 'lietuvių kalba',
-      code: 'lt'
-  },
-  {
-      description: 'Latviešu',
-      code: 'lv'
-  },
-  {
-      description: 'Nederlands',
-      code: 'nl'
-  },
-  {
-      description: 'Norsk nynorsk',
-      code: 'nn-NO'
-  },
-  {
       description: 'Polski',
       code: 'pl'
-  },
-  {
-      description: 'Português (Europeu)',
-      code: 'pt-PT'
-  },
-  {
-      description: 'Română',
-      code: 'ro'
-  },
-  {
-      description: 'slovenčina',
-      code: 'sk'
-  },
-  {
-      description: 'Slovenščina',
-      code: 'sl'
-  },
-  {
-      description: 'Svenska',
-      code: 'sv-SE'
-  },
-  {
-      description: 'Українська',
-      code: 'uk'
   }
 ];
