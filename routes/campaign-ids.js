cb4c5df71e21f465138c0278da560ca1262ea4ea
ScottDowne <scott.downe@gmail.com>
2018-06-22 15:23:05 -0400

    Update strings and campaign ids

--- routes/campaign-ids.js
+++ routes/campaign-ids.js
@@ -3,12 +3,12 @@
 const DEFAULT_LOCALE = 'GB';
 
 const COPYRIGHT_CAMPAIGN_IDS = {
-  'GB': 2, // UK English
-  'FR': 3, // French
-  'DE': 4, // German
-  'IT': 5, // Italian
-  'PL': 6, // Polish
-  'ES': 7  // Spanish
+  'GB': 13, // UK English
+  'FR': 12, // French
+  'DE': 11, // German
+  'IT': 10, // Italian
+  'PL': 9, // Polish
+  'ES': 8  // Spanish
 };
 
 module.exports = function getCopyrightCampaignID(locale) {
